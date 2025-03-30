754c663677fdd4bc124bb964f4137deb1519efaf
Iven Hsu <ivenvd@gmail.com>
2025-03-30 22:03:33 +0800

    添加 fish-autovenv

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -12,6 +12,7 @@ set -gx VISUAL $EDITOR
 set -gx PAGER less -RFXM
 set -gx PYTHONIOENCODING utf-8
 set -gx no_proxy localhost,127.0.0.1
+set -gx autovenv_announce false
 
 if test -f /etc/profile.d/99-wukong.sh; and type -q replay
     replay source /etc/profile.d/99-wukong.sh
